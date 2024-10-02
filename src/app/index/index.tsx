0d85794e2e7e085da0d1612250de242a3c76e74b
Jose Martinez <juniorjose1925@gmail.com>
2024-10-02 18:21:57 -0300

    initial commit

---
+++ src/app/index/index.tsx
@@ -0,0 +1,16 @@
+import { Text, View } from "react-native"
+
+import { styles } from "./styles"
+
+
+export default function Index() {
+  return (
+    <View style={ styles.container}>
+        <Text style= { styles.title}>Hello </Text>
+        <Text style= { styles.title}>React native.!</Text>
+      
+    </View>
+  )
+}
+
+
